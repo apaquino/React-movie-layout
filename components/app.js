0196777bf82cd65b0e7ee1430a6e82d11cf9e47d
Armand <apaquino@gmail.com>
2015-12-04 17:15:33 -0800

    initial commit

---
+++ components/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>React with webpack and babel 6 starter project ready for ES2015</h1>
+    )
+  }
+}
+
+export default App;
