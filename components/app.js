e2b48e10c5c534f56ef401086065fd31bd443211
Armand <apaquino@gmail.com>
2015-12-07 19:47:56 -0800

    made List a dumb pure function, made app a smart component

--- components/app.js
+++ components/app.js
@@ -3,12 +3,30 @@ import Header from './header.js';
 import Poster from './poster.js';
 import List from './list.js';
 
+const SOURCE = "http://api.themoviedb.org/3/discover/movie?api_key=9e1b08f9af16f8d7c20c0dd0aeb4749a&year=2015&sort_by=revenue.desc";
+
 class App extends Component {
+	constructor (props) {
+		super(props)
+		this.state = { data: [] }
+	}
+
+	loadMovies () {
+		fetch(SOURCE)
+			.then(response => response.json())
+			.then(data => this.setState({ data: data['results'] }))
+			.catch(err => console.error(this.props.source, err.toString()))
+	}
+
+	componentDidMount () {
+		this.loadMovies()
+	}
+
 	render() {
 		return (
 			<div>
 				<Header/>
-				<List source="http://api.themoviedb.org/3/discover/movie?api_key=9e1b08f9af16f8d7c20c0dd0aeb4749a&year=2015&sort_by=revenue.desc"/>
+				<List data={this.state.data} />
 			</div>
 			)
 	}
--- components/list.js
+++ components/list.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import Poster from './poster.js';
 
-class List extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { data: [] }
-  }
+let List = (props) => {
+  let posts = props.data.map((movie, id) => <Poster key={id} movie={movie} />);
 
-  loadMovies () {
-  fetch(this.props.source)
-    .then(response => response.json())
-    .then(data => this.setState({ data: data['results'] }))
-    .catch(err => console.error(this.props.source, err.toString()))
-  }
-
-  componentDidMount () {
-    this.loadMovies()
-  }
-
-	render() {
-    let posts = this.state.data.map((movie, id) => <Poster key={id} movie={movie} />);
-
-    return (
-      <div>
-        {posts}
-      </div>
-    )
-	}
+  return (
+    <div>
+      {posts}
+    </div>
+  )
 }
 
 export default List;
