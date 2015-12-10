161c66b83d05733fcbd45b411950e1e0f124a717
Armand <apaquino@gmail.com>
2015-12-10 14:35:58 -0800

    code styling

--- stores/ActorStore.js
+++ stores/ActorStore.js
@@ -6,35 +6,35 @@ let _actors = [];
 
 class ActorStore extends EventEmitter {
   constructor(props) {
-      super(props);
+    super(props);
 
-      AppDispatcher.register(action => {
-        switch (action.actionType) {
-        case ActionTypes.RECEIVE_ACTORS:
-          //clear _actors or it will accumalate after each click
-          _actors = []
-          action.actors.cast.slice(0,4).forEach(actor => {
-            _actors.push({profile_path: actor.profile_path, id: actor.id});
-          });
-          this.emit("CHANGE");
-          break;
-        default:
-          break;
-        }
-      });
-    }
-    // getters
-    getActors() {
-      return _actors;
-    }
-    // listeners
-    startListening(callback) {
-      this.on("CHANGE", callback);
-    }
+    AppDispatcher.register(action => {
+      switch (action.actionType) {
+      case ActionTypes.RECEIVE_ACTORS:
+        //clear _actors or it will accumalate after each click
+        _actors = []
+        action.actors.cast.slice(0,4).forEach(actor => {
+          _actors.push({profile_path: actor.profile_path, id: actor.id});
+        });
+        this.emit("CHANGE");
+        break;
+      default:
+        break;
+      }
+    });
+  }
+  // getters
+  getActors() {
+    return _actors;
+  }
+  // listeners
+  startListening(callback) {
+    this.on("CHANGE", callback);
+  }
 
-    stopListening(callback) {
-      this.removeListener("CHANGE", callback);
-    }
+  stopListening(callback) {
+    this.removeListener("CHANGE", callback);
+  }
 }
 
 export default new ActorStore();
--- stores/MovieStore.js
+++ stores/MovieStore.js
@@ -6,31 +6,31 @@ let _movies = [];
 
 class MovieStore extends EventEmitter {
   constructor(props) {
-      super(props);
+    super(props);
 
-      AppDispatcher.register(action => {
-        switch (action.actionType) {
-        case ActionTypes.RECEIVE_MOVIES:
-          _movies = action.movies;
-          this.emit("CHANGE");
-          break;
-        default:
-          break;
-        }
-      });
-    }
-    // getters
-    getMovies() {
-      return _movies;
-    }
-    // listeners
-    startListening(callback) {
-      this.on("CHANGE", callback);
-    }
+    AppDispatcher.register(action => {
+      switch (action.actionType) {
+      case ActionTypes.RECEIVE_MOVIES:
+        _movies = action.movies;
+        this.emit("CHANGE");
+        break;
+      default:
+        break;
+      }
+    });
+  }
+  // getters
+  getMovies() {
+    return _movies;
+  }
+  // listeners
+  startListening(callback) {
+    this.on("CHANGE", callback);
+  }
 
-    stopListening(callback) {
-      this.removeListener("CHANGE", callback);
-    }
+  stopListening(callback) {
+    this.removeListener("CHANGE", callback);
+  }
 }
 
 export default new MovieStore();
