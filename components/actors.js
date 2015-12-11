d70c277ec50386184b705f1f56c4e6ae57e5c628
Armand <apaquino@gmail.com>
2015-12-10 21:16:57 -0800

    Update actors.js

--- components/actors.js
+++ components/actors.js
@@ -4,49 +4,49 @@ import ActorActions from '../actions/ActorActions';
 import ActorStore from '../stores/ActorStore';
 
 class Actors extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {
-			actors: ActorStore.getActors()
-		};
-	}
+  constructor (props) {
+    super(props);
+    this.state = {
+      actors: ActorStore.getActors()
+    };
+  }
 
-	componentDidMount() {
-		let url = `http://api.themoviedb.org/3/movie/${this.props.id}/credits?api_key=9e1b08f9af16f8d7c20c0dd0aeb4749a`;
-		ActorActions.retrieveActors(url);
-		ActorStore.startListening(this._onFluxChange.bind(this));
-	}
+  componentDidMount() {
+    let url = `http://api.themoviedb.org/3/movie/${this.props.id}/credits?api_key=9e1b08f9af16f8d7c20c0dd0aeb4749a`;
+    ActorActions.retrieveActors(url);
+    ActorStore.startListening(this._onFluxChange.bind(this));
+  }
 
-	componentWillUnmount() {
-		ActorStore.stopListening(this._onFluxChange.bind(this));
-	}
+  componentWillUnmount() {
+    ActorStore.stopListening(this._onFluxChange.bind(this));
+  }
 
-	createActorImages() {
-		return this.state.actors.map(actor => {
-			return (
-				<img className="actor"
-					   key={actor.id}
-						 src={`//image.tmdb.org/t/p/w300/${actor.profile_path}`}>
-				</img>)
-		})
-	}
+  createActorImages() {
+    return this.state.actors.map(actor => {
+      return (
+        <img className="actor"
+             key={actor.id}
+             src={`//image.tmdb.org/t/p/w300/${actor.profile_path}`}>
+	</img>)
+  })
+}
 
-	_getActorsState() {
-		return ActorStore.getActors();
-	}
+  _getActorsState() {
+    return ActorStore.getActors();
+  }
 
-	_onFluxChange() {
-		this.setState({actors: this._getActorsState()});
-	}
+  _onFluxChange() {
+    this.setState({actors: this._getActorsState()});
+  }
 
-	render() {
-		return (
-			<div>
-				<h1>Actors Here {this.props.id}</h1>
-				{this.state.actors ? this.createActorImages() : "loading ..."}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>Actors Here {this.props.id}</h1>
+	{this.state.actors ? this.createActorImages() : "loading ..."}
+	</div>
+    )
+  }
 }
 
 export default Actors;
