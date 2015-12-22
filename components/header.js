a652b8f5b54541ec610ee1dfd6d647b894d6ece6
Armand <apaquino@gmail.com>
2015-12-22 07:23:10 -0800

    change header h3 title

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ const Header = () => {
 	  <div className="row">
 	    <div id="top1" className="col-lg-12">
 	      <h3>Top 20 Movies of 2015 by Revenue</h3>
-	      <h4>KödTest Top Movies provides you with trusted cinema data tracking millions of films across</h4>
+	      <h4>2015 Top Movies provides you with trusted cinema data tracking millions of films across</h4>
 	      <h4> all levels of the industry.</h4>
 	      <p>
 	        <a href="http://themoviedb.org/" target="_blank">
