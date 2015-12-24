98704974ee933d18a1aea7c6fba307b72f5d8184
Armand <apaquino@gmail.com>
2015-12-23 23:34:07 -0800

    change path for app component

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './container/app.js';
+import App from './containers/app.js';
 import {Provider} from 'react-redux';
 import configureStore from './stores/reduxConfigureStore';
 
