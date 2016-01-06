e9b29f93d45147ad4b1ffda851dfb75c1580bdde
Armand <apaquino@gmail.com>
2016-01-06 08:58:32 -0800

    refactor to use arrow function

--- components/poster.js
+++ components/poster.js
@@ -20,34 +20,26 @@ class Poster extends Component {
     this.state = { showModal: false }
   }
 
-  open() {
-    this.setState({ showModal: true });
-  }
-
-  close() {
-    this.setState({ showModal: false });
-  }
-
 	render() {
     let url = `//image.tmdb.org/t/p/w500/${this.props.movie.poster_path}`;
     let popover = <Popover id="Popover" title="popover">very popover. such engagement</Popover>;
     let tooltip = <Tooltip id="Toolipt" >wow.</Tooltip>;
     return (
-      <div onClick={this.open.bind(this)} className="col-lg-3 col-md-3 col-sm-6 col-xs-6 nopadding space">
+      <div onClick={() => this.setState({ showModal: true })} className="col-lg-3 col-md-3 col-sm-6 col-xs-6 nopadding space">
         <div className="wrapper">
-        <div className="poster">
-          <img className="posterImage" src= {url}/>
-        </div>
-        <div className="overlay-text">
-          <p> {this.props.movie.original_title}</p>
-          <p><span>{this.props.movie.vote_average}</span>/10
-          <span>({this.props.movie.vote_count} votes)</span></p>
-        </div>
+          <div className="poster">
+            <img className="posterImage" src= {url}/>
+          </div>
+          <div className="overlay-text">
+            <p> {this.props.movie.original_title}</p>
+            <p><span>{this.props.movie.vote_average}</span>/10
+            <span>({this.props.movie.vote_count} votes)</span></p>
+          </div>
         </div>
         {this.state.showModal ? <PosterModal {...this.props}
                                   showModal={this.state.showModal}
                                   url={url}
-                                  handleClose={this.close.bind(this)}
+                                  handleClose={() => this.setState({ showModal: false })}
                                 /> : null
         }
       </div>
