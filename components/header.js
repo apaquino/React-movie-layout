de0947d07f4e7a170289ad9c8c41221ce3ea5132
Armand <apaquino@gmail.com>
2016-02-13 00:59:38 -0800

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Header = () => {
   return (
-	  <div className="row">
-	    <div id="top1" className="col-lg-12">
-	      <h3>Top 20 Movies of 2015 by Revenue</h3>
-	      <h4>Top Movies provides you with trusted cinema data tracking millions of films across</h4>
-	      <h4> all levels of the industry.</h4>
-	      <p>
-	        <a href="http://themoviedb.org/" target="_blank">
-	          <button className="btn btn-default">Get access to this data</button>
-	        </a>
-	      </p>
-	    </div>
-	  </div>
+    <div className="row">
+      <div id="top1" className="col-lg-12">
+        <h3>Top 20 Movies of 2015 by Revenue</h3>
+        <h4>Top Movies provides you with trusted cinema data tracking millions of films across</h4>
+        <h4> all levels of the industry.</h4>
+        <p>
+          <a href="http://themoviedb.org/" target="_blank">
+            <button className="btn btn-default">Get access to this data</button>
+          </a>
+        </p>
+      </div>
+    </div>
   )
 }
 
